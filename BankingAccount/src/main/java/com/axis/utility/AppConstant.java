38e030cb8fc7e3719264bfdea279e6c347dd2cc1
aja0719 <90253743+aja0719@users.noreply.github.com>
2023-04-06 13:24:50 +0530

    Delete BankingAccount/src directory

--- BankingAccount/src/main/java/com/axis/utility/AppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.axis.utility;
-
-public interface AppConstant {
-
-	public static final String CUSTOMER_ID_NOT_FOUND_MESSAGE = "No customerId present for search";
-	
-	public static final String INSUFFIENT_BALANCE="Insuffient Balance";
-}
